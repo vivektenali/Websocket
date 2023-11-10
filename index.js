3b3d814aae925805653ebb2b8f008103d73aad58
vivektenali <vivektenali@gmail.com>
2023-11-10 22:24:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(PORT, () => console.log(`Server Started at PORT:9000`));
